71f0b9f7882392da704d45ddc1e27e758a70d4a8
Fabian Adame <fabian.kinil@cbtis72.edu.mx>
2022-02-19 15:18:11 -0500

    etNombre.text

--- app/src/main/java/com/cbtis/miprimeraapp/MainActivity.kt
+++ app/src/main/java/com/cbtis/miprimeraapp/MainActivity.kt
@@ -2,11 +2,24 @@ package com.cbtis.miprimeraapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    }
+    fun validaDato(): Boolean{
+            val etNombre = findViewById<EditText>(R.id.etNombre)
+            val nombreUsuario = etNombre.text
+            if(nombreUsuario.isNullOrEmpty()){
+                return false
+            }
+
+
+        return true
     }
 }
