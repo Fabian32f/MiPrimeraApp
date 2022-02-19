763db2e6272cb9ef6584618b5cfccad0c968c825
Fabian Adame <fabian.kinil@cbtis72.edu.mx>
2022-02-19 15:41:02 -0500

    Toast Hola!

--- app/src/main/java/com/cbtis/miprimeraapp/MainActivity.kt
+++ app/src/main/java/com/cbtis/miprimeraapp/MainActivity.kt
@@ -5,11 +5,22 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bSaludar = findViewById<Button>(R.id.bSaludar)
+        bSaludar.setOnClickListener (View.OnClickListener {
+
+            if (){
+
+            }
+
+            Toast.makeText( this,  "Hola!", Toast.LENGTH_LONG).show()
+        })
 
     }
     fun validaDato(): Boolean{
