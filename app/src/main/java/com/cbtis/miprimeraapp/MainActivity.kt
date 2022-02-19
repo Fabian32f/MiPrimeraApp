9dfc4e589bff6c4cf8f7c6c22ba202e0b91399a4
Fabian Adame <fabian.kinil@cbtis72.edu.mx>
2022-02-19 16:09:47 -0500

    CbDev final

--- app/src/main/java/com/cbtis/miprimeraapp/MainActivity.kt
+++ app/src/main/java/com/cbtis/miprimeraapp/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.Toast
 
@@ -14,11 +15,22 @@ class MainActivity : AppCompatActivity() {
 
         val bSaludar = findViewById<Button>(R.id.bSaludar)
         val etNombre = findViewById<EditText>(R.id.etNombre)
+        val cbDev = findViewById<CheckBox>(R.id.cbDeveloper)
 
         bSaludar.setOnClickListener (View.OnClickListener {
 
             if (validaDato()){
-                Toast.makeText( this,  "Bienvenido "+ etNombre.text, Toast.LENGTH_LONG).show()
+                if (cbDev.isChecked){
+                    Toast.makeText( this,  "Bienvenido "+ etNombre.text+", se que eres un desarrollador" ,Toast.LENGTH_LONG).show()
+
+                }else{
+                    Toast.makeText( this,  "Bienvenido "+ etNombre.text, Toast.LENGTH_LONG).show()
+
+                }
+
+              }else{
+                Toast.makeText( this,  "Escribe tu nombre para saludarte :/", Toast.LENGTH_SHORT).show()
+
             }
 
 
